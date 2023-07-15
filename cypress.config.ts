581cc0ddeb166be70dd55a24070a9bef16c92075
b0ratt <maciej.boratyn94@gmail.com>
2023-07-15 18:33:19 +0200

    enable e2e workflow

--- cypress.config.ts
+++ cypress.config.ts
@@ -5,6 +5,13 @@ export default defineConfig({
 		baseUrl: 'http://localhost:3000',
 		viewportHeight: 1080,
 		viewportWidth: 1920,
+		reporter: 'cypress-mochawesome-reporter',
+		reporterOptions: {
+			reportDir: 'cypress/reports',
+			overwrite: false,
+			html: false,
+			json: true,
+		},
 		setupNodeEvents(on, config) {
 			// implement node event listeners here
 		},

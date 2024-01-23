f5821995eb669a0c2cefe96786956160087c954b
b0ratt <maciej.boratyn94@gmail.com>
2024-01-23 20:27:05 +0100

    workflow dispatch

--- cypress/e2e/order/order-flow-small.cy.ts
+++ cypress/e2e/order/order-flow-small.cy.ts
@@ -172,8 +172,6 @@ describe('Order flow', () => {
             productPrice = _price.toString();
           })
           .then(() => {
-            const sanitizedPrice = productPrice.replace('$', '');
-
             header.assertCartIndicator(1).clickCartBtn();
             cartPage
               .assertCartContainsProduct(productName)

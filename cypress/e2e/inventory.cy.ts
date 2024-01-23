7d05daf99acb57a2ffd86faae17926b7afe709f9
b0ratt <65670977+b0ratt@users.noreply.github.com>
2024-01-23 19:04:42 +0100

    Pre commit hook (#6)
    * pre-commit hooks
    
    * add husky with code formatters

--- cypress/e2e/inventory.cy.ts
+++ cypress/e2e/inventory.cy.ts
@@ -4,89 +4,89 @@ import { InventoryPage } from '../page-objects/InventoryPage';
 import { credentials } from '../support/credentials';
 
 describe('Inventory tests', () => {
-	const inventoryPage = new InventoryPage();
-	const header = new Header();
-	const footer = new Footer();
+  const inventoryPage = new InventoryPage();
+  const header = new Header();
+  const footer = new Footer();
 
-	context('Standard user', () => {
-		const username = credentials.username[0];
+  context('Standard user', () => {
+    const username = credentials.username[0];
 
-		beforeEach('Login and navigate to inventory page', () => {
-			cy.setCookie('session-username', username);
-			inventoryPage.visit();
-		});
+    beforeEach('Login and navigate to inventory page', () => {
+      cy.setCookie('session-username', username);
+      inventoryPage.visit();
+    });
 
-		it('Should verify inventory content', () => {
-			header
-				.assertInventoryHeader()
-				.assertMenuButton()
-				.assertCartButton()
-				.assertSortContainer()
-				.assertSortingOptions();
+    it('Should verify inventory content', () => {
+      header
+        .assertInventoryHeader()
+        .assertMenuButton()
+        .assertCartButton()
+        .assertSortContainer()
+        .assertSortingOptions();
 
-			inventoryPage
-				.assertInventoryContainerVisible()
-				.assertInventoryItemsVisible()
-				.assertInventoryItemsContent();
+      inventoryPage
+        .assertInventoryContainerVisible()
+        .assertInventoryItemsVisible()
+        .assertInventoryItemsContent();
 
-			footer
-				.assertFooterVisible()
-				.assertSocialMediaLogo()
-				.assertCopyrightsText();
-		});
+      footer
+        .assertFooterVisible()
+        .assertSocialMediaLogo()
+        .assertCopyrightsText();
+    });
 
-		it('Should verify sorting by name', () => {
-			const sortingOptions = {
-				nameAZ: 'Name (A to Z)',
-				nameZA: 'Name (Z to A)',
-			};
+    it('Should verify sorting by name', () => {
+      const sortingOptions = {
+        nameAZ: 'Name (A to Z)',
+        nameZA: 'Name (Z to A)',
+      };
 
-			header.assertSortingOptions();
+      header.assertSortingOptions();
 
-			Object.values(sortingOptions).forEach((option) => {
-				header.sortInventoryItems(option);
+      Object.values(sortingOptions).forEach((option) => {
+        header.sortInventoryItems(option);
 
-				cy.get('.inventory_item_name').then((items) => {
-					const originalNames = items
-						.map((index, element) => Cypress.$(element).text())
-						.get();
-					const sortedNames = [...originalNames].sort();
-					const descendingSortedNames = [...originalNames].sort().reverse();
+        cy.get('.inventory_item_name').then((items) => {
+          const originalNames = items
+            .map((index, element) => Cypress.$(element).text())
+            .get();
+          const sortedNames = [...originalNames].sort();
+          const descendingSortedNames = [...originalNames].sort().reverse();
 
-					option === sortingOptions.nameAZ
-						? expect(originalNames).to.deep.equal(sortedNames)
-						: expect(originalNames).to.deep.equal(descendingSortedNames);
-				});
-			});
-		});
+          option === sortingOptions.nameAZ
+            ? expect(originalNames).to.deep.equal(sortedNames)
+            : expect(originalNames).to.deep.equal(descendingSortedNames);
+        });
+      });
+    });
 
-		it('Should verify sorting by price', () => {
-			const sortingOptions = {
-				priceLowHigh: 'Price (low to high)',
-				priceHighLow: 'Price (high to low)',
-			};
+    it('Should verify sorting by price', () => {
+      const sortingOptions = {
+        priceLowHigh: 'Price (low to high)',
+        priceHighLow: 'Price (high to low)',
+      };
 
-			header.assertSortingOptions();
+      header.assertSortingOptions();
 
-			Object.values(sortingOptions).forEach((option) => {
-				header.sortInventoryItems(option);
+      Object.values(sortingOptions).forEach((option) => {
+        header.sortInventoryItems(option);
 
-				cy.get('.inventory_item_price').then((items) => {
-					const originalPrices = items
-						.map((index, element) => Cypress.$(element).text().replace('$', ''))
-						.get();
-					const sortedNames = [...originalPrices].sort(
-						(a, b) => parseFloat(a) - parseFloat(b)
-					);
-					const descendingSortedNames = [...originalPrices].sort(
-						(a, b) => parseFloat(b) - parseFloat(a)
-					);
+        cy.get('.inventory_item_price').then((items) => {
+          const originalPrices = items
+            .map((index, element) => Cypress.$(element).text().replace('$', ''))
+            .get();
+          const sortedNames = [...originalPrices].sort(
+            (a, b) => parseFloat(a) - parseFloat(b),
+          );
+          const descendingSortedNames = [...originalPrices].sort(
+            (a, b) => parseFloat(b) - parseFloat(a),
+          );
 
-					option === sortingOptions.priceLowHigh
-						? expect(originalPrices).to.deep.equal(sortedNames)
-						: expect(originalPrices).to.deep.equal(descendingSortedNames);
-				});
-			});
-		});
-	});
+          option === sortingOptions.priceLowHigh
+            ? expect(originalPrices).to.deep.equal(sortedNames)
+            : expect(originalPrices).to.deep.equal(descendingSortedNames);
+        });
+      });
+    });
+  });
 });

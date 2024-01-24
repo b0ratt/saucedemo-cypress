0ac955b85d95cae93b931719c7d80fb2f7ac480d
b0ratt <65670977+b0ratt@users.noreply.github.com>
2024-01-24 16:31:40 +0100

    Fix reports assets (#15)
    * fix reports
    
    * fix reports

--- cypress.config.ts
+++ cypress.config.ts
@@ -4,31 +4,31 @@ const path = require('path');
 require('dotenv').config({ path: path.resolve('', '.env') });
 
 export default defineConfig({
-	reporter: 'mochawesome',
-	screenshotsFolder: 'cypress/mochawesome-report/screenshots',
-	videosFolder: 'cypress/mochawesome-report/videos',
-	reporterOptions: {
-		useInlineDiffs: true,
-		embeddedScreenshots: true,
-		reportDir: 'cypress/results/mochawesome',
-		reportFilename: '[name].html',
-		overwrite: true,
-		html: true,
-		json: true,
-	},
-	env: {
-		LOGIN: process.env.CYPRESS_USERNAME,
-		PASSWORD: process.env.CYPRESS_PASSWORD,
-		LOCKED_USER: process.env.CYPRESS_LOCKER_USER,
-		PROBLEM_USER: process.env.CYPRESS_PROBLEM_USER,
-		GLITCH_USER: process.env.CYPRESS_PERFORMANCE_GLITCH_USER,
-	},
-	e2e: {
-		baseUrl: 'http://localhost:3000',
-		viewportHeight: 1080,
-		viewportWidth: 1920,
-		setupNodeEvents(on, config) {
-			// implement node event listeners here
-		},
-	},
+  reporter: 'mochawesome',
+  screenshotsFolder: 'cypress/mochawesome/screenshots',
+  videosFolder: 'cypress/mochawesome/videos',
+  reporterOptions: {
+    useInlineDiffs: true,
+    embeddedScreenshots: true,
+    reportDir: 'cypress/results/',
+    reportFilename: '[name].html',
+    overwrite: true,
+    html: true,
+    json: true,
+  },
+  env: {
+    LOGIN: process.env.CYPRESS_USERNAME,
+    PASSWORD: process.env.CYPRESS_PASSWORD,
+    LOCKED_USER: process.env.CYPRESS_LOCKER_USER,
+    PROBLEM_USER: process.env.CYPRESS_PROBLEM_USER,
+    GLITCH_USER: process.env.CYPRESS_PERFORMANCE_GLITCH_USER,
+  },
+  e2e: {
+    baseUrl: 'http://localhost:3000',
+    viewportHeight: 1080,
+    viewportWidth: 1920,
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+  },
 });
